054fdd7b274039c5268e38851451a225e166187f
Will Chappell <wtchappell@gmail.com>
2009-12-24 19:54:54 -0600

    Changed background colors to black, darkgrey, and darkorange.

--- theme.lua
+++ theme.lua
@@ -18,9 +18,9 @@ theme.font      = "terminus 8"
 theme.fg_normal = "#DCDCCC"
 theme.fg_focus  = "#F0DFAF"
 theme.fg_urgent = "#CC9393"
-theme.bg_normal = "#3F3F3F"
-theme.bg_focus  = "#1E2320"
-theme.bg_urgent = "#3F3F3F"
+theme.bg_normal = "black"
+theme.bg_focus  = "darkgrey"
+theme.bg_urgent = "darkred"
 -- }}}
 
 -- {{{ Borders
